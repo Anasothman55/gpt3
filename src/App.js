65150ad31b9fd92ba6eb28ec4ae667eccb4bdc02
anas <anasothman582@hmail.com>
2024-07-24 23:00:04 +0300

    change 4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React from 'react'
 import { Article,Brand,Cta,Feature,Navbar } from './components/index.js'
 import {Blog,Featurec,Footer,Header,Possibility,WhatGPT3} from './containers/index.js'
 
+
+import './App.css';
 const App = () => {
   return (
     <>
-      
+      <div className='App rtx'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand/>
+        <WhatGPT3 />
+        <Featurec />
+        <Possibility />
+        <Cta/>
+        <Blog/>
+        <Footer/>
+      </div>
     </>
   )
 }
