79f44926f72cab9d77a660f1de846853b744dd5a
anas <anasothman582@hmail.com>
2024-07-26 13:33:44 +0300

    change 10

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -18,7 +18,6 @@ const Header = () => {
             <img src={people} alt="people" />
             <p>1,600 people requested access a visit in last 24 hours</p>
           </div>
-
         </div>
         <div className='gpt3__header-image'>
           <img src={ai} alt="AI_Image" />
